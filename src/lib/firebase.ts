3f3aca297bb35325dd048ec8b7b2278a2567dbc8
in-th3-l00p <indianucode0303@gmail.com>
2024-11-22 15:18:41 +0200

    added firebase project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9zBD9plhhbrq6dyd1CVBVfo8M7I6XrAo",
-  authDomain: "viainfo-f1b18.firebaseapp.com",
-  projectId: "viainfo-f1b18",
-  storageBucket: "viainfo-f1b18.firebasestorage.app",
-  messagingSenderId: "282517369332",
-  appId: "1:282517369332:web:3a77568ad3bf05d18b09a1"
+  apiKey: "AIzaSyDsTYsmxlXIWOyE4VShGrTh7QvT6_sCHWY",
+  authDomain: "viainfo2-5ce64.firebaseapp.com",
+  projectId: "viainfo2-5ce64",
+  storageBucket: "viainfo2-5ce64.firebasestorage.app",
+  messagingSenderId: "876154912959",
+  appId: "1:876154912959:web:073d4a6051488354a1060a"
 };
 
 const app = initializeApp(firebaseConfig);
