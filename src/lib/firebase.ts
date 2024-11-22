87b754e3000e2380aad0d43f8527228a97611025
in-th3-l00p <indianucode0303@gmail.com>
2024-11-22 15:30:52 +0200

    changed firebase instance once again

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsTYsmxlXIWOyE4VShGrTh7QvT6_sCHWY",
-  authDomain: "viainfo2-5ce64.firebaseapp.com",
-  projectId: "viainfo2-5ce64",
-  storageBucket: "viainfo2-5ce64.firebasestorage.app",
-  messagingSenderId: "876154912959",
-  appId: "1:876154912959:web:073d4a6051488354a1060a"
+  apiKey: "AIzaSyC0P4WvFALzZaN267DmoaK3_AGK5qXu9H4",
+  authDomain: "viainfo-657bf.firebaseapp.com",
+  projectId: "viainfo-657bf",
+  storageBucket: "viainfo-657bf.firebasestorage.app",
+  messagingSenderId: "674118957306",
+  appId: "1:674118957306:web:a475e38ed465070a2ab143"
 };
 
 const app = initializeApp(firebaseConfig);
